0108c258629b26f3a8221c0d5aa171c0c60a0af5
Unknown <antoinedelise976@hotmail.fr>
2017-03-19 11:41:41 +0100

    Leaflet Add
    Attention ça a du refaire un build sur m'a config, il faudra peut-être avoir ses build en local et mettre seulement le src dans le git (en pensant à mettre soiy-même les bons plugins et config) si ça marche plus chez toi.

--- Appli1/client/main.js
+++ Appli1/client/main.js
@@ -26,6 +26,9 @@ Template.liste.helpers({
 	'selectedColis': function(){
 		var selected_Colis = Session.get('selected_Colis');
 		return CoListe.findOne({ _id: selected_Colis });
+	},
+	'mapa': function(){
+		return 0;
 	}
 });
 
@@ -79,8 +82,14 @@ Template.addColisForm.events({
 		*/
 	}
 });
-
-
+/*
+if (Meteor.isClient) {
+    L.Icon.Default.imagePath = '/packages/bevanhunt_leaflet/images/';
+    var map = L.map('map');
+  }
+  if (Meteor.isClient) {
+    L.tileLayer.provider('Stamen.Watercolor').addTo(map);
+  }*/
 /* Un colis
 *_id
 *CreatedBy
@@ -102,3 +111,69 @@ _id
 Name
 Colis
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+Meteor.startup(function() {
+  /*$(window).resize(function() {
+    $('#map').css('height', window.innerHeight - 82 - 45);
+  });*/
+  $(window).resize(); // trigger resize event 
+});
+ 
+// create marker collection
+var Markers = new Meteor.Collection('markers');
+
+Meteor.subscribe('markers');
+
+Template.mapa.rendered = function() {
+  L.Icon.Default.imagePath = '/packages/bevanhunt_leaflet/images/';
+
+  var map = L.map('map', {
+    doubleClickZoom: false
+  }).setView([49.25044, -123.137], 13);
+
+  L.tileLayer.provider('OpenStreetMap.Mapnik').addTo(map);
+
+  map.on('dblclick', function(event) {
+    Markers.insert({latlng: event.latlng});
+  });
+
+  // add clustermarkers
+  var markers = L.markerClusterGroup();
+  map.addLayer(markers);
+
+  var query = Markers.find();
+  query.observe({
+    added: function (document) {
+      var marker = L.marker(document.latlng).addTo(map)
+        .on('click', function(event) {
+          map.removeLayer(marker);
+          Markers.remove({_id: document._id});
+        });
+       markers.addLayer(marker);
+    },
+    removed: function (oldDocument) {
+      layers = map._layers;
+      var key, val;
+      for (key in layers) {
+        val = layers[key];
+        if (val._latlng) {
+          if (val._latlng.lat === oldDocument.latlng.lat && val._latlng.lng === oldDocument.latlng.lng) {
+            //map.removeLayer(val);
+          }
+        }
+      }
+    }
+  });
+};
