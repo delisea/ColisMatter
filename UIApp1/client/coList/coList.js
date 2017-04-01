4d710729504f2f998070d7d391e666e868198e48
Unknown <antoinedelise976@hotmail.fr>
2017-04-01 11:32:26 +0200

    UIApp1 Home UI uncompleted

--- UIApp1/client/coList/coList.js
+++ UIApp1/client/coList/coList.js
@@ -1,8 +1,4 @@
-$(document).ready(function () {
-
-	$('.star').on('click', function () {
-      $(this).toggleClass('star-checked');
-    });
+/*$(document).ready(function () {
 
     $('.ckbox label').on('click', function () {
       $(this).parents('tr').toggleClass('selected');
@@ -10,6 +6,7 @@ $(document).ready(function () {
 
     $('.btn-filter').on('click', function () {
       var $target = $(this).data('target');
+	  console.log($target);
       if ($target != 'all') {
         $('.table tr').css('display', 'none');
         $('.table tr[data-status="' + $target + '"]').fadeIn('slow');
@@ -18,4 +15,22 @@ $(document).ready(function () {
       }
     });
 
- });
+ });*/
+
+Template.coList.events({
+	'click .ckbox label': function(event, t){
+		//console.log(event.target);
+		//event.target.parents('tr').toggleClass('selected');
+		$(event.currentTarget).parent().parent().parent().toggleClass('selected');
+	},
+	'click .btn-filter': function(event, t){
+		console.log("ON" + event.target.getAttribute('data-target'));
+		var $target = event.target.getAttribute('data-target');
+		if ($target != 'all') {
+			$('.table tr').css('display', 'none');
+			$('.table tr[data-status="' + $target + '"]').fadeIn('slow');
+		} else {
+			$('.table tr').css('display', 'none').fadeIn('slow');
+		}
+	}
+});
